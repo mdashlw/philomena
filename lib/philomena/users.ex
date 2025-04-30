5b606b45c03c9a388ae4c6369c9c7e8f9a564bc0
mdashlw <mdashlw@gmail.com>
2025-04-30 09:27:47 +0800

    Index filter ids on users

--- lib/philomena/users.ex
+++ lib/philomena/users.ex
@@ -872,6 +872,7 @@ defmodule Philomena.Users do
     user
     |> User.force_filter_changeset(user_params)
     |> Repo.update()
+    |> reindex_after_update()
   end
 
   @doc """
@@ -887,6 +888,7 @@ defmodule Philomena.Users do
     user
     |> User.unforce_filter_changeset()
     |> Repo.update()
+    |> reindex_after_update()
   end
 
   @doc """
@@ -1057,7 +1059,7 @@ defmodule Philomena.Users do
     |> Search.reindex(User)
   end
 
-  defp reindex_after_update(result) do
+  def reindex_after_update(result) do
     case result do
       {:ok, user} ->
         reindex_user(user)
