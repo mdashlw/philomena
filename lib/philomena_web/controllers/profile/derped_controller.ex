b5bc13ec80cd783ed12f985d5901aecf186b175c
mdashlw <mdashlw@gmail.com>
2026-01-07 12:34:27 +0400

    nvm don't want that

--- lib/philomena_web/controllers/profile/derped_controller.ex
+++ lib/philomena_web/controllers/profile/derped_controller.ex
@@ -270,39 +270,6 @@ defmodule PhilomenaWeb.Profile.DerpedController do
           with_ties: true
       )
 
-    global_top_images = %{
-      faves:
-        Repo.all(
-          from i in Image,
-            where: not i.hidden_from_users,
-            where: i.first_seen_at >= ^@start_of_year and i.first_seen_at <= ^@end_of_year,
-            order_by: [desc: i.faves_count],
-            limit: 5,
-            with_ties: true,
-            preload: [:sources, tags: :aliases]
-        ),
-      score:
-        Repo.all(
-          from i in Image,
-            where: not i.hidden_from_users,
-            where: i.first_seen_at >= ^@start_of_year and i.first_seen_at <= ^@end_of_year,
-            order_by: [desc: i.score],
-            limit: 5,
-            with_ties: true,
-            preload: [:sources, tags: :aliases]
-        ),
-      comments:
-        Repo.all(
-          from i in Image,
-            where: not i.hidden_from_users,
-            where: i.first_seen_at >= ^@start_of_year and i.first_seen_at <= ^@end_of_year,
-            order_by: [desc: i.comments_count],
-            limit: 5,
-            with_ties: true,
-            preload: [:sources, tags: :aliases]
-        )
-    }
-
     user_visits =
       Repo.one(
         from s in "derped_user_visits",
@@ -618,7 +585,6 @@ defmodule PhilomenaWeb.Profile.DerpedController do
       global_top_source_changers: global_top_source_changers,
       global_top_added_tags: global_top_added_tags,
       global_top_removed_tags: global_top_removed_tags,
-      global_top_images: global_top_images,
       user_visits: user_visits,
       user_images: user_images,
       user_comments: user_comments,
