64dbbd6257cb4f4874d22c683f222e2197ac503e
mdashlw <mdashlw@gmail.com>
2026-01-17 12:50:53 +0400

    fix

--- lib/philomena_query/search.ex
+++ lib/philomena_query/search.ex
@@ -527,7 +527,8 @@ defmodule PhilomenaQuery.Search do
     entries = Enum.map(results["hits"]["hits"], &{String.to_integer(&1["_id"]), &1})
 
     entries =
-      if not is_nil(definition.rel) and abs(definition.rel) > 1 do
+      if not is_nil(definition.rel) and
+           (abs(definition.rel) > 1 or (is_nil(definition.cursor) and definition.rel == 1)) do
         expected_size =
           min(definition.page_number * definition.page_size, count) -
             max((definition.page_number - 1) * definition.page_size, 0)
